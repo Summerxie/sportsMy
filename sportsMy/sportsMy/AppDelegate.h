14e347f6f0e6bd7fa8e430cb937e6134eeae139f
Summer <omychic@126.com>
2017-04-21 09:52:18 +0800

    集成pod

---
+++ sportsMy/sportsMy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sportsMy
+//
+//  Created by Summer on 17/4/21.
+//  Copyright © 2017年 omychic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sportsMy/sportsMy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sportsMy
+//
+//  Created by Summer on 17/4/21.
+//  Copyright © 2017年 omychic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
